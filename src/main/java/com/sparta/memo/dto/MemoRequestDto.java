3e444b66bc22ab878705cd7fa87ededcd1250baf
hangeunhyeong <hgh7546@naver.com>
2024-06-06 00:56:16 +0900

    JPA Auditing 적용

--- src/main/java/com/sparta/memo/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/memo/dto/MemoRequestDto.java
@@ -1,7 +1,6 @@
 package com.sparta.memo.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class MemoRequestDto {
--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -1,6 +1,5 @@
 package com.sparta.memo.entity;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.memo.dto.MemoRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -12,7 +11,7 @@ import lombok.Setter;
 @Table(name = "memo") //매핑할 테이블의 이름을 지정
 @NoArgsConstructor //기본생성자 애너테이션
 @Entity //JPA가 관리할 수 있는 Entity 클래스 지정
-public class Memo {
+public class Memo extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //메모끼리 구분하기 위한 것
